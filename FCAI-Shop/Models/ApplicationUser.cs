2f652e5c1456a53e7abe68ba5ce862163ffa80bd
Belal Hamdy <belalhezzat@gmail.com>
2020-04-11 22:04:05 +0200

    Added azure database connection string

--- FCAI-Shop/Models/ApplicationUser.cs
+++ FCAI-Shop/Models/ApplicationUser.cs
@@ -59,10 +59,16 @@ namespace FCAI_Shop.Models
     }
     public class ApplicationDbContext : System.Data.Entity.DbContext
     {
-        public ApplicationDbContext() : base("FCAI_Shop")
+        private const string AzureDatabase =
+            "Server=tcp:web-api.database.windows.net,1433;Initial Catalog=FCAI-Shop;Persist Security Info=False;User ID=belal;Password=FCAI-shop1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        private const string LocalDatabase = "FCAI_Shop"; // (LocalDb)\MSSQLLocalDB
+        public ApplicationDbContext() : base(AzureDatabase)
         {
             Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
         }
+
+
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Admin> Admins{ get; set; }
         public DbSet<User> Users { get; set; }
